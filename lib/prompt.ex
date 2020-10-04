23f691093cd237478a5091536153103e1bc9d6e2
Matt Silbernagel <silbermm@gmail.com>
2020-10-03 23:16:41 -0400

    add option for defaulting to no

--- lib/prompt.ex
+++ lib/prompt.ex
@@ -7,7 +7,9 @@ defmodule Prompt do
   import IO
 
   @doc """
-  Display a Y/n prompt
+  Display a Y/n prompt.
+ 
+  Sets 'Y' as the the default answer, allowing the user to just press the enter key. To make 'n' the default answer pass the option `default_answer: :no`
 
   ## Examples
 
@@ -15,10 +17,15 @@ defmodule Prompt do
   Send the email? (Y/n): Y
   iex> :ok
 
+  iex> Prompt.yes_or_no("Send the email?", [default_answer: :no], fn -> :ok end)
+  Send the email? (y/N):
+  iex> :error
+
   """
   @spec yes_or_no(String.t(), keyword(), (() -> term())) :: term()
   def yes_or_no(question, options, yes_handler, no_handler \\ fn -> :error end) do
-    write(ANSI.default_color() <> "#{question} (Y/n): ")
+    default_answer = Keyword.get(options, :default_answer, :yes) 
+    write(ANSI.default_color() <> "#{question} #{yes_no_text(default_answer)} ")
 
     case read(:stdio, :line) do
       :eof -> :error
@@ -26,6 +33,9 @@ defmodule Prompt do
       answer -> evaluate_yes_or_no(answer, question, options, yes_handler, no_handler)
     end
   end
+
+  defp yes_no_text(:yes), do: "(Y/n):"
+  defp yes_no_text(:no), do: "(y/N):"
 
   defp evaluate_yes_or_no(answer, question, options, yes_handler, no_handler) do
     answer
@@ -42,4 +52,6 @@ defmodule Prompt do
     puts("Unknown answer")
     yes_or_no(question, options, yes_handler, no_handler)
   end
+
+
 end
--- test/prompt_test.exs
+++ test/prompt_test.exs
@@ -8,6 +8,20 @@ defmodule PromptTest do
              assert result == :ok
            end) == "\e[39mSend the email? (Y/n): "
   end
+  
+  test "handle yes or no question - default" do
+    assert capture_io("\n", fn ->
+             result = Prompt.yes_or_no("Send the email?", [], fn -> :ok end, fn -> :error end)
+             assert result == :ok
+           end) == "\e[39mSend the email? (Y/n): "
+  end
+
+  test "handle yes or no question - default to no" do
+    assert capture_io("n", fn ->
+             result = Prompt.yes_or_no("Send the email?", [default_answer: :no], fn -> :ok end, fn -> :error end)
+             assert result == :error
+           end) == "\e[39mSend the email? (y/N): "
+  end
 
   test "handle yes or no - no" do
     assert capture_io("n", fn ->
