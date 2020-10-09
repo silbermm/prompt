bc4d7cec4e13898e4f16158f3421f3743353c553
Matt Silbernagel <silbermm@gmail.com>
2020-10-08 20:00:19 -0400

    add yest or no question

--- lib/prompt.ex
+++ lib/prompt.ex
@@ -13,45 +13,39 @@ defmodule Prompt do
 
   ## Examples
 
-  iex> Prompt.yes_or_no("Send the email?", [], fn -> :ok end)
+  iex> Prompt.yes_or_no("Send the email?")
   Send the email? (Y/n): Y
-  iex> :ok
+  iex> :yes
 
-  iex> Prompt.yes_or_no("Send the email?", [default_answer: :no], fn -> :ok end)
-  Send the email? (y/N):
-  iex> :error
+  iex> Prompt.yes_or_no("Send the email?", default_answer: :no)
+  Send the email? (y/N): [enter]
+  iex> :no
 
   """
-  @spec yes_or_no(String.t(), keyword(), (() -> term())) :: term()
-  def yes_or_no(question, options, yes_handler, no_handler \\ fn -> :error end) do
-    default_answer = Keyword.get(options, :default_answer, :yes) 
+  @spec yes_or_no(String.t(), keyword()) :: :yes | :no | :error
+  def yes_or_no(question, opts \\ []) do
+    default_answer = Keyword.get(opts, :default_answer, :yes) 
     write(ANSI.default_color() <> "#{question} #{yes_no_text(default_answer)} ")
 
     case read(:stdio, :line) do
       :eof -> :error
-      {:error, _reason} -> :error 
-      answer -> evaluate_yes_or_no(answer, question, options, yes_handler, no_handler)
+      {:error, _reason} -> :error
+      answer -> evaluate_yes_or_no(answer, question, opts)
     end
   end
 
   defp yes_no_text(:yes), do: "(Y/n):"
   defp yes_no_text(:no), do: "(y/N):"
 
-  defp evaluate_yes_or_no(answer, question, options, yes_handler, no_handler) do
+  defp evaluate_yes_or_no(answer, question, opts) do
     answer
     |> String.trim()
     |> String.downcase()
-    |> _evaluate_yes_or_no(yes_handler, no_handler, question, options)
+    |> _evaluate_yes_or_no(question, opts)
   end
 
-  defp _evaluate_yes_or_no("y", yes_handler, _no_handler, _, _), do: yes_handler.()
-  defp _evaluate_yes_or_no("n", _yes_handler, no_handler, _, _), do: no_handler.()
-  defp _evaluate_yes_or_no("", yes_handler, _no_handler, _, _), do: yes_handler.()
-
-  defp _evaluate_yes_or_no(_, yes_handler, no_handler, question, options) do
-    puts("Unknown answer")
-    yes_or_no(question, options, yes_handler, no_handler)
-  end
-
-
+  defp _evaluate_yes_or_no("y", _, _), do: :yes
+  defp _evaluate_yes_or_no("n", _, _), do: :no
+  defp _evaluate_yes_or_no("", _, opts), do: Keyword.get(opts, :default_answer, :yes) 
+  defp _evaluate_yes_or_no(_, question, opts), do: yes_or_no(question, opts)
 end
