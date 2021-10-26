af0fe32c9722f19266da7d293cf83001613a9dc9
Matt Silbernagel <matt@silbernagel.dev>
2021-10-25 20:34:39 -0400

    up version to 0.6.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Prompt.MixProject do
     [
       app: :prompt,
       description: "A terminal toolkit and a set of helpers for building console applications.",
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.10",
       package: package(),
       source_url: "https://github.com/silbermm/prompt",
