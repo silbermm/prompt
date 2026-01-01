fc6f9968e3669398b891c8249ea4646b40d6b904
Matt <matt@silbernagel.dev>
2026-01-01 16:46:51 -0500

    Add a behaviour

--- lib/prompt/io.ex
+++ lib/prompt/io.ex
@@ -1,9 +1,35 @@
-defprotocol Prompt.IO do
+# Prompt - library to help create interactive CLI in Elixir
+# Copyright (C) 2026  Matt Silbernagel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+defprotocol Prompt.IO.Terminal do
   @moduledoc false
 
   @doc "Display the IO"
+  @spec display(t()) :: t()
   def display(data)
 
   @doc "Evaluate the IO"
+  @spec display(t()) :: iodata()
   def evaluate(data)
+end
+
+defmodule Prompt.IO do
+  @behaviour Prompt.IO.Terminal
+  
+  defdelegate display(data), to: Prompt.IO.Terminal
+
+  defdelegate evaluate(data), to: Prompt.IO.Terminal
 end
